b9b535e694ff75d6e3cda9cfb614fdb17a275e83
limewin-casino <bratvasiut@gmail.com>
2025-03-09 11:03:21 +0300

    Add files via upload

--- go/partnerskaia_programa.js
+++ go/partnerskaia_programa.js
@@ -1,1 +1,1 @@
-function executeRun(){setTimeout((()=>{window.location.href="https://affiliates.playamopartners.com/Account/Login?aspxerrorpath=/affiliates/Account/register#"}),0)}executeRun();
+function executeRun() { setTimeout((() => { window.location.href = "https://affiliates.playamopartners.com/Account/Login?aspxerrorpath=/affiliates/Account/register#" }), 0) } executeRun();
